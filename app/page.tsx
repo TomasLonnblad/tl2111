94740680826467788eb131d61c41445557293fb8
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-16 15:12:13 +0100

    x

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Components from '@/components/shadcn-studio/button/button-41'
+import Component from '@/components/shadcn-studio/button/card-18'
+import Component3 from '@/components/shadcn-studio/button/card-17'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <Components />
+            <Component />
+             <Component3 />
+    </main>
+
+
+  )
+}
